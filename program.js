c3975ecb84ecf8bec1ca51115e398bcc8a6762ac
Sara-Jain <Sara_Jain@mckinsey.com>
2022-02-12 19:59:25 +0530

    feat: use of template strings

--- program.js
+++ program.js
@@ -1,1 +1,8 @@
-console.log("HELLO ES6");
+// Exercise 1
+// console.log("HELLO ES6");
+
+//Exercise 2
+let name = process.argv[2];
+console.log(`Hello, ${name}!
+Your name lowercased is "${name.toLowerCase()}".`)
+
