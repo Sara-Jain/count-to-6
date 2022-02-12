3879a89cc4308d6657285256f3779da21a083276
Sara-Jain <Sara_Jain@mckinsey.com>
2022-02-12 22:18:56 +0530

    feat: use tagged template string

--- program.js
+++ program.js
@@ -48,8 +48,27 @@
 // };
 
 //Exercise 9
-module.exports = (inputString, numberOfExclamations = inputString.length) => {
-    const exclamations = ('!').repeat(numberOfExclamations);
-    const sentence = inputString + exclamations;
+// module.exports = (inputString, numberOfExclamations = inputString.length) => {
+//     const exclamations = ('!').repeat(numberOfExclamations);
+//     const sentence = inputString + exclamations;
+//     return sentence;
+// };
+
+//Exercise 10
+console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
+
+function html(pieces, ...subs) {
+    var sentence = pieces[0];
+    for (var i = 0; i < subs.length; i++) {
+        sentence += escape(subs[i]) + pieces[i + 1];
+    }
     return sentence;
-};
+}
+
+function escape(s) {
+    return s.replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/'/g, "&apos;")
+        .replace(/"/g, "&quot;");
+}
